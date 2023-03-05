9035661aaf5ef58c2a26e80ebaf1b72e4a5d5b19
nic <nrhoda14@gmail.com>
2023-03-05 17:55:01 -0600

    completed typeDefs.js and started on resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,7 +3,31 @@ const { User } =require('./../models');
 const { signToken } = require('./../utils/auth');
 
 const resolvers = {
-    //deal with resolvers later
-}
+    Mutation: {
+        createUser: async (parent, args) => {
+            try {
+                const user = await User.create(args);
+                const token = signToken(user);
+                return { token, user };           
+            } catch (err) {
+                console.error(err);
+            }
+        },
+        login: async (parent, {username, email, password}) => {
+            const user = await User.findOne({ $or: [{ email }, { username }]});
+            if(!user) {
+                throw new AuthenticationError('user not found');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+            if(!correctPw) {
+                throw new AuthenticationError('incorrect username or password');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+},
+};
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,47 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-type
+type Query {
+    me: User
+}
+
+type Mutation {
+    login(username: String!, email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input:): User
+    removeBook(bookId: ):User
+}
+
+type User {
+    _id: 
+    username: String!
+    email: String!
+    bookCount: ID!
+    savedBooks: [Book]
+}
+
+type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    title: String
+}
+
+input SavedBook {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: Strin
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
 `;
 
 module.exports = typeDefs;
