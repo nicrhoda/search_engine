510b01cdc80a20c8d38c723a78e5d580146ed5d9
nic <nrhoda14@gmail.com>
2023-03-06 07:24:09 -0600

    fixed syntax errors in typeDefs.js and added me query to resolvers, also made changes to versions in package.json

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,47 +1,46 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-type Query {
+  type Query {
     me: User
-}
+  }
 
-type Mutation {
-    login(username: String!, email: String!, password: String!): Auth
+  type Mutation {
+    login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(input:): User
-    removeBook(bookId: ):User
-}
+    saveBook(input: savedBook!): User
+    removeBook(bookId: ID!): User
+  }
 
-type User {
-    _id: 
-    username: String!
-    email: String!
-    bookCount: ID!
+  type User {
+    _id: ID!
+    username: String
+    email: String
+    bookCount: Int
     savedBooks: [Book]
-}
+  }
 
-type Book {
+  type Book {
     bookId: String
     authors: [String]
     description: String
     title: String
     image: String
-    title: String
-}
+  }
 
-input SavedBook {
+  input savedBook {
     bookId: String
     authors: [String]
     description: String
     title: String
     image: String
-    link: Strin
-}
+    link: String
+  }
 
-type Auth {
+  type Auth {
     token: ID!
     user: User
-}
+  }
 `;
 
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 const path = require('path');
-const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: false }));
@@ -17,7 +18,7 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, './../client/build')));
 }
 
 app.get('/', (req, res) => {
