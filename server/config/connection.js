b6fbe205a69920a3570312abfd67874d2be26d1f
nic <nrhoda14@gmail.com>
2023-03-08 18:31:30 -0600

    replaced server package.json file and worked with connection.js to try to establish server connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
 })
 .catch ((err) => { console.error(err) });
 
